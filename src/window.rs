74835b9d3a661476752e01d065ba220799d9bace
meloencoding <mellon@ziggo.nl>
2023-04-18 19:01:57 +0200

    added FormatMessagA for error descriptions

--- src/window.rs
+++ src/window.rs
@@ -1,4 +1,7 @@
-use windows::{Win32::{UI::{WindowsAndMessaging::{DefWindowProcA, CreateWindowExA, WS_CAPTION, WS_SYSMENU, ShowWindow, LoadCursorW, IDC_ARROW, WNDCLASSEXA, RegisterClassExA, WM_CLOSE, PostQuitMessage, WS_MINIMIZEBOX, HICON, WM_DESTROY, DestroyWindow, WNDCLASS_STYLES, MSG, GetMessageA, TranslateMessage, DispatchMessageA}}, Foundation::{HWND, WPARAM, LPARAM, LRESULT, HINSTANCE, BOOL, GetLastError}, System::{LibraryLoader::{GetModuleHandleA}}, Graphics::Gdi::HBRUSH}, core::{PCSTR}};
+use windows::{Win32::{UI::{WindowsAndMessaging::{DefWindowProcA, CreateWindowExA, WS_CAPTION, WS_SYSMENU, ShowWindow, LoadCursorW, IDC_ARROW, WNDCLASSEXA, RegisterClassExA, WM_CLOSE, PostQuitMessage, WS_MINIMIZEBOX, HICON, WM_DESTROY, DestroyWindow, WNDCLASS_STYLES, MSG, GetMessageA, TranslateMessage, DispatchMessageA}}, Foundation::{HWND, WPARAM, LPARAM, LRESULT, HINSTANCE, BOOL, GetLastError, WIN32_ERROR, SetLastError}, System::{LibraryLoader::{GetModuleHandleA}, Diagnostics::Debug::{FormatMessageA, FORMAT_MESSAGE_FROM_SYSTEM, FORMAT_MESSAGE_ALLOCATE_BUFFER, FORMAT_MESSAGE_IGNORE_INSERTS, FormatMessageW}}, Graphics::Gdi::HBRUSH}, core::{PCSTR, PSTR, PWSTR}, imp::heap_free};
+
+
+
 pub struct Window {
     pub instance: HINSTANCE,
     pub class_name: PCSTR,
@@ -115,21 +118,98 @@ impl Window {
         unsafe { DispatchMessageA(&mut self.msg_buffer) };
         None
     }
-
+    
     pub fn get_exit_codes(&self) {
-        println!("exit with codes getResult: {:?}, wParam: {}, lastError: {}", self.last_result.0, self.msg_buffer.wParam.0, unsafe { GetLastError().0 });
+        /*
+        Computate the WIN32_ERROR to the description of the error. For more info check: https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes
+        */
+        
+        // To test this function you could uncomment this piece of code and see what it returns.
+        // unsafe { SetLastError(windows::Win32::Foundation::WIN32_ERROR(5)) };
+        
+        // If you want to see what langid it would use if you set dwLanguageId to 0, uncomment this:
+        // println!("{}", unsafe {windows::Win32::Globalization::GetUserDefaultLangID()});
+
+        let err_code: u32 = unsafe { GetLastError().0 }; // Get the last WIN32_ERROR and get the id from it (u32)
+        let mut err_buffer: *mut u8 = std::ptr::null_mut(); // Create a buffer for windows where it should store the error message
+        if err_code == 0 {
+            // If the error code == 0, there is no error. So there is no need for priting a succes error :)
+            return println!("Succesfull exit with codes: last getResult: {:?}, wParam: {}", self.last_result.0, self.msg_buffer.wParam.0)
+        }
+
+        let err_msg_lenght: u32 = unsafe {
+            FormatMessageA( 
+                /*
+                    Formats a message string. The function requires a message definition as input. 
+
+                    The function finds the message definition in a message table resource based on 
+                    a message identifier (HRESULT/GetLastError()) and a language identifier (LCID). The function copies the 
+                    formatted message text to an output buffer, processing any embedded insert 
+                    sequences if requested.
+
+                    For more info see: https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-formatmessage
+                 */   
+                FORMAT_MESSAGE_FROM_SYSTEM | // Use system message tables to retrieve error text
+                FORMAT_MESSAGE_ALLOCATE_BUFFER, // Allocate buffer on local heap for error text
+                None, // Location of the message definition. We use the systems error table so it has to be None
+                err_code, // The Errorcode you want a description about
+                0, // LCID (language code identifier) ->
+                /* 
+                    This one is a bit weird. In the description the FormatMessage 
+                    function it says we need an LANGID but there is nothing like that in the windows crate. This crate uses a LCID. 
+                    0 means that it will use your system languague. 1033 means US. 
+                    For more info see: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-lcid/70feba9f-294e-491e-b6eb-56532684c37f
+                 */
+                PSTR(&mut err_buffer as *mut _ as *mut _), // Man... this took me ages to get working. ->
+                /*
+                    A pointer to a buffer that receives the null-terminated string that specifies the formatted message.
+                    This buffer cannot be larger than 64K bytes.
+                      ---
+                    We first create a mutable null pointer and set the type to a u8 like this: 
+                        let mut err_buffer: *mut u8 = std::ptr::null_mut(); 
+                    Then we use the PSTR constructor to create a pointer to a null-terminated string of 8-bit Windows (ANSI) characters.
+                    like this: 
+                        PSTR();
+                    Then we put in a mutable reference to the error_buffer and cast it to an mutable pointer (I have no clue how and why this works)
+                        PSTR(&mut err_buffer as *mut _ as *mut _);
+                 */
+                0, 
+                /*
+                    If the FORMAT_MESSAGE_ALLOCATE_BUFFER flag is not set, this parameter specifies the size of the output buffer, in TCHARs. If 
+                    FORMAT_MESSAGE_ALLOCATE_BUFFER is set, this parameter specifies the minimum number of TCHARs to allocate for an output buffer.
+                 */
+                None 
+                /*
+                    An array of values that are used as insert values in the formatted message.
+                    For more info see: https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-formatmessage#parameters
+                 */
+            )
+        };
+
+
+        if err_msg_lenght == 0 { // If the message buffer is empty, there is no available error description
+            /*
+                Could be caused by an invalid error code or an invalid or not correctly installed LCID
+             */
+            return println!("Unable to find error description. Errorcode: {}", err_code);
+        }
+
+        // If there is an error, print all the return codes,
+        println!("Unsuccesfull exit with codes getResult: {:?}, wParam: {}, lastError: {}", self.last_result.0, self.msg_buffer.wParam.0, unsafe { GetLastError().0 });
+        // and print out the description of the code
+        let slice = unsafe { std::slice::from_raw_parts(err_buffer, (err_msg_lenght - 2) as _).to_vec()};
+        return println!("Errorcode {}: {:?}", err_code, String::from_utf8(slice).unwrap());
+
     }
 
     extern "system" fn wndproc(hwnd: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
         /*
-
             It is very hard to explain how this works without typing a lot of text so i'll just refer you to
             the great video by ChiliTomatoNoodle (https://youtu.be/UUbXK4G_NCM). It explains how the window
             messages work and how to build a good system around it. 
 
             For more info about wndproc see: https://learn.microsoft.com/en-us/windows/win32/api/winuser/nc-winuser-wndproc
             And for a list with all the messages see: https://wiki.winehq.org/List_Of_Windows_Messages 
-
          */
         
         unsafe {
